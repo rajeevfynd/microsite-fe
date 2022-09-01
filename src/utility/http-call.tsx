cbddbc6f00ded133fecd9489d318c6c648083566
sridharswain <sridharswain@fynd-external.com>
2022-09-01 16:55:06 +0530

    directory structure added

--- src/utility/http-call.tsx
+++ src/utility/http-call.tsx
@@ -1,2 +1,3 @@
 // All code related to remote calls comes up
 // It should be central utility to patch headers such as cookies before any api call
+// access-token, Cookie // Start a loader, exception handling {error: true, data: {message: "", errorCode: ""}}, 
