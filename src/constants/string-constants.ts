60d7f5d461f54d363b2e42ff8f553dbe59b696bf
Jayanthi Kumar <jayanthikumar@fynd-external.com>
2022-09-06 14:51:33 +0530

    custom url instead of ant.d default url

--- src/constants/string-constants.ts
+++ src/constants/string-constants.ts
@@ -1,5 +1,8 @@
 // titile of our website
 export const SITE_TITLE='Microsite'
 
-//footer content of our website
+// footer content of our website
 export const SITE_FOOTER='Microsite | 3P Market Place'
+
+// defaul url for profile pic
+export const DEFAUL_PRO_PIC_URL="https://i.ibb.co/mbVk9tG/profile-pic.png"
