e3d2fbd46912036b4f677864a4272951995b08b2
Jayanthi Kumar <jayanthikumar@fynd-external.com>
2022-09-07 18:15:24 +0530

    button to open modal

--- src/screens/learning-development/new-employee-center/induction/views/welcome.tsx
+++ src/screens/learning-development/new-employee-center/induction/views/welcome.tsx
@@ -7,34 +7,38 @@ import { Button, Col, Input, Modal, Row, message } from 'antd';
 import { isUserAuthorized } from '../../../../../service/user-service';
 
 export const Welcome = () => {
-  const [open, setOpen] = React.useState(false);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
   const [url, setUrl] = React.useState(getWelcomeFileUrl())
   return (
     <>
-        <h1>Welcome to Jio</h1>
-        <div className='video-player'>
-            <ReactPlayer controls url={getWelcomeFileUrl()}></ReactPlayer>
-        </div>
+      <h1>Welcome to Jio</h1>
+      <div className='video-player'>
+        <ReactPlayer controls url={getWelcomeFileUrl()}></ReactPlayer>
+      </div>
 
-        { isUserAuthorized (['ADMIN-LND','ADMIN-GLOBAL']) && <>
-        <div className='update-welcome'>
-          <a onClick={()=>{setOpen(!open)}}>Update Welcome File <EditOutlined /></a>
-        </div>
-        <Modal title="Update Welcome Message File URL" visible={open} footer={null} onCancel={() => setOpen(!open)}>
-          <Input.Group size='large' className='modal-welcome-url-update'>
-            <Row>
-              <Col span={15}>
-                <Input defaultValue={url} onChange={(e) => setUrl(e.target.value)}></Input>
-              </Col>
-              <Col>
-                <Button type='primary' size='large' 
-                onClick = {() => { let status=setWelcomeFileUrl(url); status.isUpdated? message.success(status.message): message.error(status.message); setOpen(!status.isUpdated)}}>Update</Button>
-              </Col>
-            </Row>
-            </Input.Group>
+      { 
+        isUserAuthorized (['ADMIN-LND','ADMIN-GLOBAL']) && 
+        <>
+          <div className='update-welcome'>
+            <Button onClick={()=>{setIsModalOpen(!isModalOpen)}}>Update File <EditOutlined /></Button>
+          </div>
+
+          <Modal title="Update Welcome Message File URL" visible={isModalOpen} footer={null} onCancel={() => setIsModalOpen(!isModalOpen)}>
+            <Input.Group size='large' className='modal-welcome-url-update'>
+              <Row>
+                <Col span={15}>
+                  <Input defaultValue={url} onChange={(e) => setUrl(e.target.value)}></Input>
+                </Col>
+                <Col>
+                  <Button type='primary' size='large' 
+                  onClick = {() => { let status=setWelcomeFileUrl(url); status.isUpdated? message.success(status.message): message.error(status.message); setIsModalOpen(!status.isUpdated)}}>Update</Button>
+                </Col>
+              </Row>
+              </Input.Group>
           </Modal>
         </>
       }
+
     </>
   )
 }
