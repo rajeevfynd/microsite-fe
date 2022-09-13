5f0693fe27e4bb8b5ba006435b65602437286bce
Harsh Vardhan <harshvardhan@fynd-external.com>
2022-09-13 14:20:56 +0530

    added course icon and course list in hero page

--- src/constants/global-constants.tsx
+++ src/constants/global-constants.tsx
@@ -1,2 +1,3 @@
+//TODO change as per development requirements
 export const CURRENT_COURSES_URL = "http://127.0.0.1:8080/microsite/lnd/current_programs/1"
 export const COMPLETED_COURSES_URL = "http://127.0.0.1:8080/microsite/lnd/completed_programs/1"
