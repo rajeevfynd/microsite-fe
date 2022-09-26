2f6adb10800dfe5ed82f33d8a5aba387b6e14c0e
sridharswain <sridharswain@fynd-external.com>
2022-09-26 11:29:43 +0530

    Authentication implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
 import * as React from "react";
-import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom" ;
+import { BrowserRouter, Navigate } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./index.css";
 import { GlobalRouter } from "./Route";
+import { useCookies } from 'react-cookie'
+import { Routes, Route } from 'react-router-dom'
+import { useQuery } from "./utility/query-utils";
+import { AUTHORISATION_PATH, AUTHORISATION_TOKEN_PARAM } from "./constants/global-constants";
 
-const App = () => {
+const ProtectedRoutes = () => {
+    const [cookies] = useCookies([AUTHORISATION_TOKEN_PARAM])
+    if (cookies[AUTHORISATION_TOKEN_PARAM] == null) {
+        window.location.href = AUTHORISATION_PATH;
+        return (<></>);
+    }
     return (
         <>
-            <BrowserRouter>
-                <GlobalRouter></GlobalRouter>
-            </BrowserRouter>
+            <GlobalRouter></GlobalRouter>
         </>
     )
+}
+
+const LoginComponent = () => {
+    const [cookies, setCookie] = useCookies([AUTHORISATION_TOKEN_PARAM])
+    const query = useQuery();
+    const token = query.get(AUTHORISATION_TOKEN_PARAM);
+    setCookie(AUTHORISATION_TOKEN_PARAM, token);
+    return (<Navigate to="/" />)
+}
+
+const App = () => {
+    return (<>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/authorize' element={<LoginComponent />} />
+                <Route path="*" element={<ProtectedRoutes />} />
+            </Routes>
+        </BrowserRouter>
+    </>)
 }
 export default App;
--- src/constants/global-constants.tsx
+++ src/constants/global-constants.tsx
@@ -1,3 +1,3 @@
-const BACKEND_URL = ""
-
-export default BACKEND_URL;
+export const AUTHORISATION_PATH: string = "/microsite/saml/auth";
+export const AUTHORISATION_HEADER: string = "Authorization";
+export const AUTHORISATION_TOKEN_PARAM: string = "auth";
