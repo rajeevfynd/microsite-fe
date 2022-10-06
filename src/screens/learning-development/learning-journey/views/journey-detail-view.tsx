462cb184f6460ad884439c6f0bccaf9be9626ee0
Jayanthi Kumar <jayanthikumar@fynd-external.com>
2022-10-06 17:02:26 +0530

    journey-details

---
+++ src/screens/learning-development/learning-journey/views/journey-detail-view.tsx
@@ -0,0 +1,56 @@
+import Button from 'antd/lib/button';
+import * as React from 'react'
+import { ArrowLeft } from 'react-bootstrap-icons';
+import { useNavigate, useParams } from 'react-router-dom'
+import { JourneyDetail } from '../../../../components/journey-detail/journey-detail';
+import { Flow } from '../../../../models/enums/flow';
+import { ProgressStatus } from '../../../../models/enums/progress-status';
+import { JourneyDetailType, ProgramType } from '../../../../models/journey-details';
+import httpInstance from '../../../../utility/http-client';
+
+export const JourneyDetailView = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [ data, setData ] = React.useState({})
+
+    const processData = (data: JourneyDetailType | any) => {
+        const processedData = processPrograms(data.programs, data.flow)
+        data.programs = processedData.programs;
+        data.progress = processedData.progress;
+        setData(data)
+      }
+    
+      const processPrograms = (programs: ProgramType[], flow: string) => {
+        if (programs && programs.length > 0) {
+          const progress = Math.round(programs.filter(program => program.status == 'COMPLETED').length * 100 / programs.length);
+          let flowKey = flow as keyof typeof Flow;
+          if (Flow[flowKey] == Flow.SEQUENCE)
+            programs.every(program => {
+              program.isActive = true;
+              let enumKey = program.status as keyof typeof ProgressStatus;
+              if (ProgressStatus[enumKey] != ProgressStatus.COMPLETED) {
+                return false;
+              }
+              return true;
+            })
+          return {
+            programs: programs,
+            progress: progress
+          };
+        }
+      }
+
+    React.useEffect( ()=>
+    {   
+        httpInstance.get('/microsite/lnd/journeys/details/'+id).then( res => {
+            console.log(res.data);
+            processData(res.data);
+        })
+    }, [])
+  return (
+    <>
+        <div><Button type='link' onClick={()=>{navigate(-1)}}>< ArrowLeft/> Back</Button></div>
+        <JourneyDetail details={data}></JourneyDetail>
+    </>
+  )
+}
---
+++ src/screens/learning-development/learning-journey/views/learning-journey-list.tsx
@@ -0,0 +1,133 @@
+import { Avatar, Card, Divider, Input, List, Skeleton } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import * as React from 'react';
+import { Search } from 'react-bootstrap-icons';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { JourneyDetailType } from '../../../../models/journey-details'; 
+import httpInstance from '../../../../utility/http-client';
+import './../index.css'
+
+
+interface JourneyPage {
+  content ?: Array<JourneyDetailType>,
+  empty ?: boolean,
+  first ?: true,
+  last ?: true,
+  number ?: number,
+  numberOfElements ?: number,
+  size ?: number,
+  totalElements ?: number,
+}
+
+export  const LearningJourneyList = () => {
+    const navigate = useNavigate();
+  const [load, setLoad] = React.useState(false)
+  const[ journeys, setJourneys] = React.useState<JourneyDetailType[]>([])
+  const [hasMore, setHasMore ] = React.useState(false)
+  const [page,setPage ] = React.useState(1)
+  
+  let key = ''
+
+  const loadMoreData = () => {
+    if (load) {
+      return;
+    }
+    setLoad(false);
+
+    httpInstance.get('/microsite/lnd/journeys/search?key='+key.toString()+'&page='+page.toString()+'&size=8').then(
+      resp => {
+        setJourneys([...journeys, ...resp.data.content])
+        setHasMore(!resp.data.last)
+        setPage(page +1)
+        setLoad(false)
+      }
+    )
+  };
+
+  const searchJourneys = () => {
+    if(load) {
+      return ;
+    }
+    setLoad(false);
+
+    httpInstance.get('/microsite/lnd/journeys/search?key='+key.toString()+'&page='+'&size=8').then(
+      resp => {
+        setJourneys([...resp.data.content])
+        setHasMore(!resp.data.last)
+        setPage(2)
+        setLoad(false)
+      }
+    )
+  }
+
+  React.useEffect(() => {
+    loadMoreData();
+  }, []);
+
+  const searchKey = (str: string) =>{
+    key = str
+    handleSearch()
+  }
+
+  const handleSearch = () => {
+    debounce(searchJourneys,500)
+  }
+
+let debounceTimer:any;
+ 
+const debounce = (callback:any, time:any) => {
+  window.clearTimeout(debounceTimer);
+  debounceTimer = window.setTimeout(callback, time);
+};
+ 
+
+  return (
+    <>
+    <h3>Learning Journey</h3>
+    <div className='search-container'>
+      <Input size='large' className='search-box' suffix={<Search/>} onChange={(e) => {searchKey(e.target.value);} } />
+    </div>
+    <div
+      id="scrollableDiv"
+      style={{
+        height: '60vh',
+        overflow: 'auto',
+        padding: '0 16px',
+        border: '1px solid rgba(140, 140, 140, 0.35)',
+      }}
+    >
+      <InfiniteScroll
+        dataLength={journeys.length}
+        next={loadMoreData}
+        hasMore={hasMore}
+        loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+        
+        scrollableTarget="scrollableDiv"
+      >
+        <List
+          grid={{gutter: 16, column: 4}}
+          dataSource={journeys}
+          renderItem={item => (
+            <List.Item onClick={()=>{navigate(item.id.toString())}} key={item.title}>
+              <Card 
+                hoverable
+                cover={
+                  <img
+                    src={item.thumbnailLink}
+                  />
+                }
+              >
+                <Meta
+                  title={item.title}
+                  description={item.description}
+                />
+              </Card>
+            </List.Item>
+          )}
+        />
+      </InfiniteScroll>
+    </div>
+    </>
+  );
+};
