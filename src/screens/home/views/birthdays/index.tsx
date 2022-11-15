1787128c44b08aa815e7bc3d570cffa3cb49f2df
Sridhar Swain <sridharswain@fynd-external.com>
2022-11-15 19:43:26 +0000

    Home enhancement

--- src/screens/home/views/birthdays/index.tsx
+++ src/screens/home/views/birthdays/index.tsx
@@ -47,9 +47,8 @@ export const BirthDays = () => {
 
     return (
         <>
-            <Card style={{ width: '100%' }}>
+            <Card className="home-card">
                 <Meta title={<div><h4>Today's Birthday</h4></div>} />
-
                 <div style={{ display: 'flex', alignItems: 'center' }}>
                     <Typography.Text disabled style={{ fontSize: "12px", width: '35%' }}>Click to wish them</Typography.Text>
                     <div style={{ width: '100%' }} >
