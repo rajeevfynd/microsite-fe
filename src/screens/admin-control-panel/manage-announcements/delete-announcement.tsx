4c4006950e28981eabefaa7880d2a09efe73c8a6
Harsh Vardhan <harshvardhan@fynd-external.com>
2022-11-15 18:43:35 +0530

    Homepage Created

---
+++ src/screens/admin-control-panel/manage-announcements/delete-announcement.tsx
@@ -0,0 +1,45 @@
+import {DeleteOutlined,SearchOutlined} from '@ant-design/icons'
+import { Button, Card } from 'antd';
+import * as React from 'react'
+import { announcementType } from '../../../models/announcementType';
+import { deleteAnnouncementService, getAnnouncement } from '../../../service/announcment-service';
+import { AnnouncementThumbnail } from './announcement-thumbnail';
+
+export const DeleteAnnouncement = (props:any) => {
+    const [announcements,setAnnouncements] = React.useState<announcementType []>([])
+    const [loadagain,setloadagain] = React.useState()
+
+    const fetchAnncouncement = React.useCallback( async()=>{
+        console.log("inside rerender")
+        let response = await getAnnouncement();
+        const announcement_list: announcementType [] = response.data
+        setAnnouncements(announcement_list)
+    },[])
+    React.useEffect(() =>{
+        fetchAnncouncement()
+    },[props,loadagain,fetchAnncouncement])
+
+    const deleteAnnouncement = async (id:any) => {
+        let resp = await deleteAnnouncementService(id);
+        fetchAnncouncement
+        setloadagain(id)
+    }
+    
+
+    return (
+        <>
+            <h5>Delete Annnouncements</h5>
+            <div>
+            {announcements && announcements.length>0 && announcements.map(announcement => (
+                    <div key={announcement.id}>
+                        <Card title = {announcement.title} extra={<Button type='text' icon={<DeleteOutlined />} onClick={()=>{deleteAnnouncement(announcement.id)}}> </Button>}>
+                            {announcement.description} 
+                            {announcement.documentId&&<AnnouncementThumbnail {...{announcement}}/>}
+                                                    
+                        </Card>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
