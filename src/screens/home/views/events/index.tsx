633b570d87f125398b6d23c6594a1920bfdb2d64
Jayanthi Kumar <jayanthikumar@fynd-external.com>
2022-12-07 11:02:34 +0530

    comit

--- src/screens/home/views/events/index.tsx
+++ src/screens/home/views/events/index.tsx
@@ -1,16 +1,18 @@
-import { Avatar, Button, Card, Carousel, Col, Row } from 'antd';
+import { CaretRightOutlined, CheckCircleFilled, CheckCircleOutlined, FieldTimeOutlined, PauseOutlined, WarningOutlined } from '@ant-design/icons';
+import { Avatar, Button, Card, Carousel, Col, Modal, Row } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import * as React from 'react';
-import { ArrowRight } from 'react-bootstrap-icons';
+import { ArrowRight, Check, PlayCircle } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router-dom';
+import { CourseDetails } from '../../../../components/course-detail/course-details';
 import { LearningEvent } from '../../../../models/enums/learning-event';
 import { getLearningEvents } from '../../../../service/event-service';
+import { getCourseById } from '../../../../service/program-service';
 import "./index.scss";
 
 type LearningEventType = {
-    id : number,
+    id : string,
     title : string,
-    thumbnailLink : string,
     type : LearningEvent
 }
 
@@ -22,14 +24,24 @@ type SurveyEventType = {
 export const Events = () => {
 
     const navigate = useNavigate()
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+    const [courseDetails, setCourseDetails] = React.useState({});
 
     const handleLearningEvent = () => {
        if(learningEvent.type == LearningEvent.JOURNEY){
-        navigate('/lnd/learning-journey/'+learningEvent.id)
+            navigate('/lnd/learning-journey/'+learningEvent.id)
        }
        else if(learningEvent.type == LearningEvent.PROGRAM){
-        navigate('/lnd/programs/'+learningEvent.id)
+            navigate('/lnd/programs/'+learningEvent.id)
        }
+       else if(learningEvent.type == LearningEvent.COURSE){
+            getCourseById(learningEvent.id).then( res =>
+                {
+                    setCourseDetails(res.data)
+                    setIsModalOpen(true)
+                }
+            )
+        }
     }
 
     const [learningEvent, setLearningEvent] = React.useState<LearningEventType>()
@@ -37,19 +49,33 @@ export const Events = () => {
 
     React.useEffect( ()=>{
         getLearningEvents().then( res=> {
+            console.log(res)
             setLearningEvent(res.data)
         })
     },[])
     return (
         <>
+            <Modal
+            title="Course Details"
+            visible={isModalOpen}
+            footer={null}
+            onCancel={()=>{setIsModalOpen(false)}}
+            width={1000}
+            style={{ top: 100 }}>
+                <CourseDetails course={courseDetails} />
+            </Modal>
             <Card className="home-card" style={{padding:0}}>
                 <Carousel autoplay pauseOnHover effect='fade'>
                     {
                         <Card
                             className='event-carousel background-yellow'>
-                            <h6 style={{color: 'white'}}>
-                                Employee survey is still pending
+                            <h6 style={{color: 'white', height:'50px'}}>
+                                "Employee Happiness Survey" is still pending
                             </h6>
+                            <div style={{color:'white', margin:'10px'}}>
+                                <FieldTimeOutlined style={{fontSize:'30px'}}/>
+                             </div>
+                            
                             <Button onClick={handleLearningEvent} type='default' className='event-link'>
                                 Go to survey 
                             </Button>
@@ -60,14 +86,25 @@ export const Events = () => {
                         <Card
                             className='event-carousel background-blue'
                         >
-                            <h6 style={{color: 'white'}}>Continue learning "{learningEvent.title}"</h6>
+                            <h6 style={{color: 'white', height:'50px'}}>
+                                Continue learning "{learningEvent.title}"
+                            </h6>
+                            <div style={{color:'white', margin:'10px'}}>
+                                <PauseOutlined style={{fontSize:'30px'}}/>
+                             </div>
+                            
                             <Button onClick={handleLearningEvent} type='default' className='event-link'>Go to {learningEvent.type.toLowerCase()} </Button>
                         </Card>
                     }
                     {   
                         <Card
                             className='event-carousel background-green'>
-                            <h6 style={{color: 'white'}}>You are all caught up!</h6>
+                            <h6 style={{color: 'white', height:'50px'}}>
+                                You are all caught up!
+                            </h6>
+                            <div style={{color:'white', margin:'10px'}}>
+                                <CheckCircleOutlined style={{fontSize:'30px'}}/>
+                             </div>
                             <Button onClick={()=>{navigate('/lnd/learning-center/lnd-hero')}} type='default' className='event-link'>Go to Learning Center </Button>
                         </Card>
                     }
