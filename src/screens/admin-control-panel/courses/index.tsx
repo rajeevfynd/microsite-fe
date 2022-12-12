3811e1524baeb0560f1188f998af917a0d3b79ac
Harsh Vardhan <harshvardhan@fynd-external.com>
2022-12-12 12:46:14 +0530

    bugfix

--- src/screens/admin-control-panel/courses/index.tsx
+++ src/screens/admin-control-panel/courses/index.tsx
@@ -80,7 +80,7 @@ export const AdminCoursePage = () => {
                     >
                     <List
                         grid={{ gutter: 10, xs: 1, sm: 1, md: 2, lg: 2, xl: 3, xxl: 4}}
-                        style={{ padding: "1%" , width:"100%"}}
+                        style={{ padding: "1%" }}
                         dataSource={courses}
                         renderItem={item => (
                             <List.Item key={item.title}>
