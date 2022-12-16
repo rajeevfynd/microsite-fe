9860d282b8016ba45948037e22929bd214ccb8a0
jay <jaymaurya@fynd-external.com>
2022-12-16 13:53:56 +0530

    [fixed] course description overflow

--- src/screens/admin-control-panel/courses/index.tsx
+++ src/screens/admin-control-panel/courses/index.tsx
@@ -101,7 +101,7 @@ export const AdminCoursePage = () => {
                                     }
                                 >
                                     <Meta
-                                    style= {{overflow:"hidden"}}
+                                    style= {{ textAlign: "center"}}
                                         title={item.title}
                                         description={item.description}
                                     />
