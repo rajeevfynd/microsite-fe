f7c7ab93df26df3b091ac7fb31e46fd4f88790e2
Jayanthi Kumar <jayanthikumar@fynd-external.com>
2022-12-20 11:41:43 +0000

    protected component

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -81,15 +81,15 @@ export const TopNavigationMenu = (props: TopNavigationMenuProps) => {
     return (
         <>
             <div className='menu-container'>
-                {props.menu.map(item => <MenuItem {...item} selected={menuPrimaryKey == item.key} onClick={() => {
+                {props.menu.map(item => !(item.hasAccess === false) && <MenuItem {...item} selected={menuPrimaryKey == item.key} onClick={() => {
                     setMenuPrimaryKey(item.key);
                     onMenuItemClick(item);
                     onPrimaryMenuItemClick(item);
-                }} />)}
+                }} />) }
             </div>
             <div className='secondary-menu'>
                 <div className='secondary-menu-container'>
-                    {secondaryItems.map(item => <SecondaryMenuItem {...item} selected={menuSecondaryKey == item.key} onClick={() => {
+                    {secondaryItems.map(item => !(item.hasAccess === false) && <SecondaryMenuItem {...item} selected={menuSecondaryKey == item.key} onClick={() => {
                         setMenuSecondaryKey(item.key);
                         onMenuItemClick(item);
                     }} />)}
--- src/utility/user-utils.tsx
+++ src/utility/user-utils.tsx
@@ -2,8 +2,17 @@ import { parseJwt } from './jwt-utils';
 import { useCookies } from 'react-cookie';
 import { AUTHORISATION_TOKEN_PARAM } from "../constants/global-constants";
 import { User } from '../models/user';
+import { Role } from '../models/enums/roles';
 
 export function getUser(): User {
     const [cookies] = useCookies([AUTHORISATION_TOKEN_PARAM]);
     return parseJwt(cookies[AUTHORISATION_TOKEN_PARAM]);
 };
+
+export const isAdmin = () => {
+    const user = getUser();
+    if(user){
+        return user.role == Role.ADMIN || user.role == Role.SUPER_ADMIN
+    } 
+    return false
+}
