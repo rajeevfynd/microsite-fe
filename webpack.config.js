042725bee781422a402e8272f03abbb0d853efe5
sridharswain <sridharswain@fynd-external.com>
2022-12-21 10:31:44 +0530

    less added

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,27 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader', // compiles Less to CSS
+          options: {
+            lessOptions: { // If you are using less-loader@5 please spread the lessOptions to options directly
+              modifyVars: {
+                'primary-color': '#00adb5',
+                'link-color': '#1DA57A',
+                'border-radius-base': '2px',
+              },
+              javascriptEnabled: true,
+            },
+          },
+        }],
+        // ...other rules
+      }
     ],
   },
   resolve: {
