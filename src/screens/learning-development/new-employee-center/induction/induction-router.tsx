97af4804af86cb29d0d258d1d7d672eb19efef0d
Jayanthi Kumar <jayanthikumar@fynd-external.com>
2022-12-27 19:34:45 +0530

    induction & welcome revamp

--- src/screens/learning-development/new-employee-center/induction/induction-router.tsx
+++ src/screens/learning-development/new-employee-center/induction/induction-router.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { Induction } from './views/induction'
 import { Welcome } from './views/welcome'
 
 export const InductionRouter = () => {
   return (
     <>
         <Routes>
-            <Route path='/welcome' element={<Welcome></Welcome>}></Route>
-            <Route path='*' element={<Navigate replace to="/lnd/new-emp-center/induction/welcome"/>}></Route>
+            <Route path='/welcome' element={<Induction />}></Route>
+            <Route path='*' element={<Navigate replace to="/lnd/new-emp-center/induction"/>}></Route>
         </Routes>
     </>
   )
